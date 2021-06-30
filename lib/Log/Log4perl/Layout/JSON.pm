766d8301069e76165d204aa05c85e2e5454fbb69
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2021-07-01 00:03:51 +0500

    Undo overwritng fix

--- lib/Log/Log4perl/Layout/JSON.pm
+++ lib/Log/Log4perl/Layout/JSON.pm
@@ -312,7 +312,7 @@ sub render {
                     $len = length $encoded;
                 }
                 else {
-                    $len = defined $v ? length $v : 0;
+                    $len = length($v) || 0;
                 }
                 next if $len <= $max_json_length/2;
 
